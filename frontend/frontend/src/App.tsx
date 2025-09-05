431112f8e8c2f0faf47b4f0eac07cf12365149be
stomaras <sptoma724@gmail.com>
2025-09-05 15:54:34 +0300

    add lazy loading

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,20 +1,22 @@
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import { useAppDispatch } from "./features/auth/hook";
 import { restoreLogin } from "./features/auth/authSlice";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import CrashTest from "./pages/CrashTest";
-import WorkerDashboard from "./components/WorkerDashboard";
-import AdminDashboard from "./components/AdminDashboard";
+
 import ProtectedRoute from "./components/ProtectedRoute";
 import * as Sentry from "@sentry/react";
 import ErrorFallback from "./components/ErrorFallback";
 import useIdleLogout from "./hooks/useIdleLogout";
 
+const LoginPage = React.lazy(() => import("./pages/LoginPage"));
+const CrashTest = React.lazy(() => import("./pages/CrashTest"));
+const WorkerDashboard = React.lazy(() => import("./components/WorkerDashboard"));
+const AdminDashboard = React.lazy(() => import("./components/AdminDashboard"));
+
 
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
-    useIdleLogout(); // enable auto-logout
+  useIdleLogout(); // enable auto-logout
 
   useEffect(() => {
     dispatch(restoreLogin()); // restore login on page load
@@ -23,28 +25,29 @@ const App: React.FC = () => {
   return (
     <Sentry.ErrorBoundary fallback={<ErrorFallback />}>
       <Router>
-        <Routes>
-          <Route path="/crash" element={<CrashTest />} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
 
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/worker-dashboard"
-            element={
-              <ProtectedRoute role="WORKER">
-                <WorkerDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin-dashboard"
-            element={
-              <ProtectedRoute role="ADMIN">
-                <AdminDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<LoginPage />} />
-        </Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/worker-dashboard"
+              element={
+                <ProtectedRoute role="WORKER">
+                  <WorkerDashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin-dashboard"
+              element={
+                <ProtectedRoute role="ADMIN">
+                  <AdminDashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<LoginPage />} />
+          </Routes>
+        </Suspense>
       </Router>
     </Sentry.ErrorBoundary>
   );
